89c30627402e8b26b80c5010d5b3987ba6390b89
David <djh111@pitt.edu>
2024-10-12 19:21:53 -0400

    this works with gradescope

--- autograding/a1/gradescope/autograder.py
+++ autograding/a1/gradescope/autograder.py
@@ -2,8 +2,8 @@ import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == "__main__":
-    suite = unittest.defaultTestLoader.discover('.')
-    with open('/results/results.json', 'w') as f:
+    suite = unittest.defaultTestLoader.discover('autograde/')
+    with open('results/results.json', 'w') as f:
         print("Starting tests")
         JSONTestRunner(visibility='visible', stream=f).run(suite)
     print("Test complete")
